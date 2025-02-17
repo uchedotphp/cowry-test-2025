ee0402be7f1cf56c2bd0d4fa8c324f9e24dca91c
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-02-17 07:50:49 +0100

    chore: install and configure Tailwind CSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    welcome
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </main>
 </template>
